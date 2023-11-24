9b01b9fc94e2a9277a66f0bb4a163e4c6fb16d28
Victor Oliveira <vggaia10@gmail.com>
2023-11-23 21:54:01 -0300

    add a shared component

---
+++ packages/ui/ChadButton/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import * as S from './styles'
+
+
+export default function ChadButton() {
+  return <S.ChadButtonContainer>Chad Button</S.ChadButtonContainer>;
+}
---
+++ packages/ui/ChadButton/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ChadButtonContainer = styled.div`
+  background: red;
+
+  padding: 20px;
+`;
